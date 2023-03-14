58ea662efa9bf615aef58ea2b14d9fa31b5d5e8b
Vullum <markus.yung.vullum@gmail.com>
2023-03-14 11:13:41 +0100

    Får ut resultater i json

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,27 +3,37 @@
 
 import { useEffect, useState } from "react"
 
-export default function Main(){
-
+export default function Main() {
   const [movie, setMovie] = useState([])
-  const [filter, setFilter] = useState([])
+  const [filter, setFilter] = useState("")
   const [result, setResult] = useState([])
 
-  const getMovie = async() =>{
-    const response = await fetch('https://www.omdbapi.com/?s=James%20Bond&type=movie&apikey=685b8bcf')
+  const getMovie = async () => {
 
-    const data = await response.json()
+    const response = await fetch(
+      "https://www.omdbapi.com/?s=James%20Bond&type=movie&apikey=685b8bcf"
+    )
 
-    setMovie(data.articles)
-
-    setResult(movie.filter(items => items.source.name === filter))
-
+    const data = await response.json();
+    
+    setMovie(data.Search);
   }
+
+  const filterMovie = () => {
+      setResult(movie.filter((item) =>
+        item.Title.toLowerCase().includes(filter.toLowerCase())
+      )) 
+  }
+
+  useEffect(() => {
+    filterMovie();
+  }, [movie, filter]);
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   console.log(result);
 
-  useEffect(() =>{
-    getMovie()
-  },[])
-
-  return <h2>Maidjskjn</h2>
+  return <h2>Maidjskjn</h2>;
 }
